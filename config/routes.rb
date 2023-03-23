2f0434948c149ac5f7b085f1915af5e7ac5e3436
harsh291199 <harshsuthar2911@gmail.com>
2023-03-23 13:36:45 +0530

    Add stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products
   post 'checkout/create', to: "checkout#create"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :webhooks, only: [:create]
 end
