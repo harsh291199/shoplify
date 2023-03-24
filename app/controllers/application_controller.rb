9d01b37d71672945d1fe770da6f3420077af50ad
harsh291199 <harshsuthar2911@gmail.com>
2023-03-24 17:28:47 +0530

    Add session based shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :initialize_session
+  before_action :load_cart
 
-  protected
+  private
 
   def after_sign_in_path_for(resource)
     root_path
@@ -9,5 +11,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
+  end
+
+  def initialize_session
+    session[:cart] ||= []
+  end
+
+  def load_cart
+    @cart = Product.find(session[:cart])
   end
 end
