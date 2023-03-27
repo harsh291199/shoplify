53ce99ea1052a3f0362f391fd450ddd94df2c575
harsh291199 <harshsuthar2911@gmail.com>
2023-03-27 15:54:43 +0530

    After successful checkout increment sales price without webhooks

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -6,14 +6,20 @@ class CheckoutController < ApplicationController
       payment_method_types: ["card"],
       line_items: @cart.collect { |item| item.to_builder.attributes! },
       mode: 'payment',
-      success_url: success_url,
+      success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: cancel_url,
     })
 
     redirect_to @session.url, allow_other_host: true
   end
 
-  def success; end
+  def success
+    @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"] })
+    @session_with_expand.line_items.data.each do |line_item|
+      product = Product.find_by(stripe_product_id: line_item.price.product)
+      product.increment!(:sales_count)
+    end
+  end
 
   def cancel; end
 end
