f3890d322d720d7dc48eb797673d0677a575637c
harsh291199 <harshsuthar2911@gmail.com>
2023-03-27 17:08:42 +0530

    Empty cart after successful transaction

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,5 @@
 class CheckoutController < ApplicationController
   def create
-    product = Product.find_by(id: params[:id])
     @session = Stripe::Checkout::Session.create({
       customer: current_user.stripe_customer_id,
       payment_method_types: ["card"],
@@ -14,9 +13,12 @@ class CheckoutController < ApplicationController
   end
 
   def success
-    @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"] })
-    @session_with_expand.line_items.data.each do |line_item|
-      product = Product.find_by(stripe_product_id: line_item.price.product)
+    if params[:session_id].present?
+      session.delete(:cart)
+      # session[:cart] = []
+      @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ["line_items"] })
+    else
+      redirect_to cancel_path, alert: "No info to display"
     end
   end
 
